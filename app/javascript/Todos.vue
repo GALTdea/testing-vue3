f13c92407e020838034f3e3467821fcd65d35b42
GALTdea <gustavovalenzuela1180@gmail.com>
2019-10-17 16:50:29 -0700

    fixing todo component bug

--- app/javascript/Todos.vue
+++ app/javascript/Todos.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- todos pertains to the props todos -->
-    <div v-bind:key"todo.id" v-for="todo in todos">
+    <div v-bind:key="todo.id" v-for="todo in todos">
       <h3>{{ todo.title }}</h3>
     </div>
   </div>
