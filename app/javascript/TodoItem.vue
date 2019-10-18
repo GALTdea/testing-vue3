808af0c9ca7bb1bae4f638c5e04c07a23c66396a
GALTdea <gustavovalenzuela1180@gmail.com>
2019-10-17 19:49:27 -0700

    .

--- app/javascript/TodoItem.vue
+++ app/javascript/TodoItem.vue
@@ -1,7 +1,10 @@
 <template>
   <!-- v-binding a css class on condition that todo.iscomplete -->
   <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
-    <p> {{todo.title}} </p>
+    <p> 
+      <!-- <input type="checkbox" v-on:change="markComplete" > -->
+      {{todo.title}} 
+    </p>
   </div>
 </template>
 
