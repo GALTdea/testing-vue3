3c18ce669252c97b8e681c4bad58c92cf78e3aa3
GALTdea <gustavovalenzuela1180@gmail.com>
2019-10-17 19:23:04 -0700

    adding todoItem component

--- app/javascript/Todos.vue
+++ app/javascript/Todos.vue
@@ -2,14 +2,19 @@
   <div>
     <!-- todos pertains to the props todos -->
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <h3>{{ todo.title }}</h3>
+      <TodoItem v-bind:todo="todo"  />
     </div>
   </div>
 </template>
 
 <script>
+  import TodoItem from './TodoItem.vue';
+
   export default {
     name: "Todos",
+    components: {
+      TodoItem
+    },
     props: ["todos"],
     
   }
