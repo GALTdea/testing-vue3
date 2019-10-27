0660020047ac143ba26036dfe785f7ea4ee23f1c
GALTdea <gustavovalenzuela1180@gmail.com>
2019-10-27 15:07:03 -0700

    fhm

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <p>This is a test</p>
-    <p>{{ message }}</p>
+    <p>{{ title }}</p>
   </div>
 </template>
 
 <script>
 export default {
-props: ['message'],
+// props: ['message'],
+props: {
+  title: String,
+  msg1: String,
+  msg2: String,
+  msg3: String,
+  }
 }
 </script>
 
@@ -17,3 +23,11 @@ p {
   text-align: center;
 }
 </style>
+
+
+
+
+<!-- t.string "title"
+    t.text "msg1"
+    t.text "msg2"
+    t.text "msg3 -->
