567d2c061aaffec1aad76faa567342b0d764a70c
GALTdea <gustavovalenzuela1180@gmail.com>
2019-10-28 15:44:51 -0700

    fhm

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,62 +1,14 @@
 <template>
   <div id="app">
-  	
-  	<p>{{ message1 }}</p>
-	  <!-- <div> -->
-	     <!-- <form @submit="addItem"> 
-	    	 <div>Message:{{ message }}</div>
-
-	      <input  v-model="message"  placeholder="Add Title...">
-	     
-	      <input type="submit" value="Submit" class="btn">
-	    </form> -->
-
-	    <!-- <input v-model="message" placeholder="edit me"> -->
-		<!-- <p>Message is: {{ message }}</p> -->
-	  <!-- </div> -->
-  	<!-- 	
-  	  <div class="msg">
-  	  	<div>title</div>
-	    {{ message.title }}
-
-	    <div>msg1</div>
-	    {{ message.msg1 }}
-
-	    <div>msg2</div>
-	    {{ message.msg2 }}
-	  </div> -->
-    
-    	
+  	<p>{{ message }}</p>
   </div>
 </template>
 
 <script>
 	export default {
-		props: [ 'message1'],
+		props: [ 'message'],
 
-		// data: {
-		//  	message: {
-		//         title: '',
-		//         msg1: '',
-		//         msg2: '',
-		//         msg3: '',
-		//       }
-		// },
-
-		// methods: {
-		// 	addItem: function() {
-		// 		this.message.push({
-		//         	id: null,
-		//         	title: '',
-		//         	msg1: '',
-		//         	msg2: '',
-		//         	msg3: '',
-		//         })
-		        
-	 //      	}
-    
-
-		// }
+		
 	}	
 
 
